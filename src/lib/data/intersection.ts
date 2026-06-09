29570a528b4f1b92be1db2f38e93a37f1ace02d7
sumukund <mukund.su@husky.neu.edu>
2026-06-09 14:43:00 -0500

    process imgs

--- src/lib/data/intersection.ts
+++ src/lib/data/intersection.ts
@@ -1,5 +1,5 @@
 const imageModules = import.meta.glob(
-  "$lib/assets/images/intersection_images/*.{png,jpg,jpeg,svg,webp}",
+  "$lib/assets/images/intersection_images/*.{png,jpg,jpeg,svg,webp,JPG}",
   { eager: true }
 );
 
@@ -41,4 +41,82 @@ export const intersectionCapstone = [
     image: images.avg_5, // Changed from hardcoded string
     size: "small",
   },
+];
+
+export const performanceImages = [
+  {
+    title: "performance 1",
+    year: "2026",
+    location: "Minneapolis, MN",
+    description: "performance 1",
+    image: images.perf_1, // Changed from hardcoded string
+    size: "small",
+  },
+  {
+    title: "performance 2",
+    year: "2026",
+    location: "Minneapolis, MN",
+    description: "performance 2",
+    image: images.perf_2, // Changed from hardcoded string
+    size: "small",
+  },
+  {
+    title: "performance 3",
+    year: "2026",
+    location: "Minneapolis, MN",
+    description: "performance 3",
+    image: images.perf_3, // Changed from hardcoded string
+    size: "small",
+  },
+  {
+    title: "performance 4",
+    year: "2026",
+    location: "Minneapolis, MN",
+    description: "performance 4",
+    image: images.perf_4, // Changed from hardcoded string
+    size: "small",
+  },
+  {
+    title: "performance 5",
+    year: "2026",
+    location: "Minneapolis, MN",
+    description: "performance 5",
+    image: images.perf_5, // Changed from hardcoded string
+    size: "small",
+  }
+];
+
+export const processImages = [
+  {
+    title: "performance 1",
+    year: "2026",
+    location: "Minneapolis, MN",
+    description: "performance 1",
+    image: images.process_3, // Changed from hardcoded string
+    size: "small",
+  },
+  {
+    title: "performance 2",
+    year: "2026",
+    location: "Minneapolis, MN",
+    description: "performance 2",
+    image: images.process_4, // Changed from hardcoded string
+    size: "small",
+  },
+  {
+    title: "performance 3",
+    year: "2026",
+    location: "Minneapolis, MN",
+    description: "performance 3",
+    image: images.process_5, // Changed from hardcoded string
+    size: "small",
+  },
+  {
+    title: "Room Setup",
+    year: "2026",
+    location: "Minneapolis, MN",
+    description: "performance 4",
+    image: images.room_setup, // Changed from hardcoded string
+    size: "small",
+  }
 ];
